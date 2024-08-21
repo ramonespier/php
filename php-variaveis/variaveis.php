07c4a159396cfb0396bd1dc16bdf973df6fb0cfe
ramonespier <ramon.coelhomelo@outlook.com>
2024-08-21 11:25:21 -0300

    test: variaveis

---
+++ php-variaveis/variaveis.php
@@ -0,0 +1,20 @@
+<?php
+//string
+$texto = "Olá, mundo";
+echo $texto . '<br>';
+
+//integer
+$inteiro = 42;
+echo $inteiro . '<br>';
+
+//float
+$decimal = 3.14;
+echo $decimal . '<br>';
+
+// boolean
+$verdadeiro = true;
+$falso = false;
+echo  $verdadeiro . '<br>';
+echo $falso . '<br>';
+
+?>
