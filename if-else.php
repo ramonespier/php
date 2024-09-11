3f3367d2f35f5ed02bc2f32ad2b2282697ecaa9d
ramonespier <ramon.coelhomelo@outlook.com>
2024-09-11 08:51:22 -0300

    feat: if-else

---
+++ if-else.php
@@ -0,0 +1,22 @@
+<?php
+
+$x = 5;
+
+if ($x > 3) {
+    echo 'O número é maior!';
+
+} else {
+    echo 'O número é menor.';
+
+}
+
+$hora = 8;
+
+if ($hora <= 12) {
+    echo '<br>É manhã';
+
+} else {
+    echo '<br>É tarde';
+}
+
+?>
