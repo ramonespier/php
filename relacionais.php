0a85f8dc7960c7e60cd9bc222c0acf7c57b14dde
ramonespier <ramon.coelhomelo@outlook.com>
2024-09-11 08:10:30 -0300

    feat:  operadores relacionais

---
+++ relacionais.php
@@ -0,0 +1,24 @@
+<?php
+
+$a = 17;
+$b = 5;
+
+$maior = ($a > $b);
+echo '<br>A maior que B: ', $maior;
+
+$menor = ($a < $b);
+echo '<br>A menor que B: ', $menor;
+
+$maiorigual = ($a >= $b);
+echo '<br>A maior ou igual que B: ', $maiorigual;
+
+$menorigual = ($a <= $b);
+echo '<br>A menor ou igual que B: ', $menorigual;
+
+$igual = ($a == $b);
+echo '<br>A igual que B: ', $igual;
+
+$diferente = ($a != $b);
+echo '<br>A diferente que B: ', $diferente;
+
+?>
