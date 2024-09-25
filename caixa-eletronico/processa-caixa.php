31bc9cfc04ad7dca518b12d36272c013fbc29acb
ramonespier <ramon.coelhomelo@outlook.com>
2024-09-25 08:08:57 -0300

    fix: correção caixa eletronico

--- caixa-eletronico/processa-caixa.php
+++ caixa-eletronico/processa-caixa.php
@@ -7,15 +7,17 @@ $saldo_restante = $saldo - $saque;
 if ($saque <= 500 && 0 < $saque) {
     if(($saque % 10) != 0) {        
         echo "O valor do saque deve ser multiplo de 10. ";
-        
-        } else {
-            echo "O saldo restante é $saldo_restante.";
 
+    } else {
+        echo "O saldo restante é $saldo_restante.";
     }
 
-}else {
-    echo "Digite apenas números.";
-    }
+} else if ($saque > 500 || $saque < 0) {
+    echo "Saldo insuficiente. <br>
+    Saldo disponível: $saldo";
 
+} else {
+     echo "Digite apenas números.";
+     };
 
-
+?>
