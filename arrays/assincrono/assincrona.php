3285b9fcd3fc7c5890549cd3328931ed1a279f44
ramonespier <ramon.coelhomelo@outlook.com>
2024-10-16 09:25:59 -0300

    transformando a atividade assincrona em uma bomboniere

--- arrays/assincrono/assincrona.php
+++ arrays/assincrono/assincrona.php
@@ -1,56 +1,76 @@
-<?php
-$doces = [
-    "Twix" => [
-        "nome" => "Twix",
-        "preco" => 3.99,
-        "descricao" => "Doce de chocolate com caramelo e nozes",
-        "foto" => "https://t4.ftcdn.net/jpg/05/50/22/45/360_F_550224506_0KRvZE9szzIOeFIWfRKzfTd8Va3GELnD.jpg" // nao entendi a parte de imagens :/
-    ],
+<!DOCTYPE html>
+<html lang="pt-br">
 
-    "Trento" => [
-        "nome" => "Trento",
-        "preco" => 4.99,
-        "descricao" => "Doce de chocolate",
-        "foto" => "https://images-na.ssl-images-amazon.com/images/I/51VGZHO8HkL.jpg"
-    ],
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bomboniere</title>
+    <link rel="stylesheet" href="doces.css">
+</head>
 
-    "Suflair" => [
-        "nome" => "Suflair",
-        "preco" => 7.99,
-        "descricao" => "Doce de chocolate aerado",
-        "foto" => "https://cdn.awsli.com.br/400x400/1957/1957771/produto/104770016/suflair-50g-cx-uohhltgdxv.png"
-    ],
+<body>
+    <div class="flex">
+        <div class="grid">
 
-    "Bis" => [
-        "nome" => "Bis",
-        "preco" => 8.99,
-        "descricao" => "Doce de pequenos chocolates, extramamente viciante",
-        "foto" => "https://cdn.dooca.store/259/products/chocolate-bis-ao-leite-1008g-lacta_640x640+fill_ffffff.jpg?v=1701099483&webp=0"
-    ],
+            <?php
+            $doces = [
+                "Twix" => [
+                    "nome" => "Twix",
+                    "preco" => 3.99,
+                    "descricao" => "Doce de chocolate com caramelo e nozes",
+                    "foto" => '<img src="./twix.jpg"></img>'
+                ],
 
-    "KitKat" => [
-        "nome" => "KitKat",
-        "preco" => 8.99,
-        "descricao" => "Doce de chocolate com biscoito ao leite",
-        "foto" => "https://t3.ftcdn.net/jpg/04/84/89/10/360_F_484891061_wO3qH6EiQVAxeRBNafqFH7K0mtLnPXmU.jpg"
-    ],
-    
-    "Talento" => [
-        "nome" => "Talento",
-        "preco" => 5.99,
-        "descricao" => "Doce de chocolate com avelã",
-        "foto" => "https://www.garoto.com.br/sites/default/files/2021-04/Talento_Choc_Avela.jpg"
-    ]
-];
+                "Trento" => [
+                    "nome" => "Trento",
+                    "preco" => 4.99,
+                    "descricao" => "Doce de chocolate",
+                    "foto" => '<img src="./trento.jpg"></img>'
+                ],
 
-foreach ($doces as $chave => $valor) {
-    if (is_array($valor)) {
-        echo "<br> $chave: <br>";
-        foreach ($valor as $descricao => $conteudo) {
-            echo "$descricao: $conteudo <br>";
-        }
+                "Suflair" => [
+                    "nome" => "Suflair",
+                    "preco" => 7.99,
+                    "descricao" => "Doce de chocolate aerado",
+                    "foto" => '<img src="./suflair.webp"></img>'
+                ],
 
-    } else {
-        echo "$chave: $valor <br>";
-    }
-}
+                "Bis" => [
+                    "nome" => "Bis",
+                    "preco" => 8.99,
+                    "descricao" => "Doce de pequenos chocolates, extramamente viciante",
+                    "foto" => '<img src="./bis.jpg"></img>'
+                ],
+
+                "KitKat" => [
+                    "nome" => "KitKat",
+                    "preco" => 8.99,
+                    "descricao" => "Doce de chocolate com biscoito ao leite",
+                    "foto" => '<img src="./kitkat.jpg"></img>'
+                ],
+
+                "Talento" => [
+                    "nome" => "Talento",
+                    "preco" => 5.99,
+                    "descricao" => "Doce de chocolate com avelã",
+                    "foto" => '<img src="./talento.avif"></img>'
+                ]
+            ];
+
+            foreach ($doces as $chave => $valor) {
+                echo  '<div class="card">';
+                echo  '<h2>' . $valor['nome'] . '</h2>';
+                echo  '<p class="descricao">' . $valor['descricao'] . '</p>';
+                echo  '<p class="preco">R$ ' . $valor['preco'] . '</p>';
+
+                echo  $valor['foto'];
+                echo  '</div>';
+            }
+            ?>
+
+        </div>
+    </div>
+
+</body>
+
+</html>
