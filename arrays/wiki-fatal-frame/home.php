f192efc0431fff343cda629440628e6781096e54
ramonespier <ramon.coelhomelo@outlook.com>
2024-10-18 08:01:01 -0300

    chore: movendo script para arquivo proprio

--- arrays/wiki-fatal-frame/home.php
+++ arrays/wiki-fatal-frame/home.php
@@ -121,26 +121,7 @@
         </button>
     </div>
 
-    <script>
-        let iAtual = 0;
-
-        function showSlide(index) {
-            const slides = document.querySelectorAll('.carrossel-item');
-            if (slides.length === 0) return;
-            if (index >= slides.length) iAtual = 0;
-            if (index < 0) iAtual = slides.length - 1;
-
-            const deslocamento = -iAtual * 100;
-            document.querySelector('.dentro-carrossel').style.transform = `translateX(${deslocamento}%)`;
-        }
-
-        function moveSlide(step) {
-            iAtual += step;
-            showSlide(iAtual);
-        }
-
-        showSlide(iAtual);
-    </script>
+    <script src="./fatal-script.js"></script>
 
 
 </body>
