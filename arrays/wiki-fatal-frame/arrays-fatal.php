6fe557fdfd3397da9f1c7fc2d8d11070aae19c17
ramonespier <ramon.coelhomelo@outlook.com>
2024-10-23 17:37:51 -0300

    test: TENTANDO criar paginas de acordo com o indice de cada array

--- arrays/wiki-fatal-frame/arrays-fatal.php
+++ arrays/wiki-fatal-frame/arrays-fatal.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 $jogos = [
     "Fatal Frame (2001)" => [
         "nome" => "Fatal Frame",
@@ -36,6 +36,75 @@ $jogos = [
         "nome" => "Fatal Frame: Maiden of Black Water (2015)",
 
         "descricao" => "O quinto título da franquia foi lançado para o Wii U em 2014 no Japão e em 2015 para os outros territórios. A história, que se passa na fictícia Montanha Hikami, concentra em três protagonistas que são atraídas por eventos sobrenaturais que acontecem na área, incluindo um ritual antigo para selar a Água Negra, uma força malévola que corrompe os espíritos circundantes que está ligado ao destino da donzela do santuário Ose Kurosawa. Em 2021, em comemoração do 20º aniversário da série, a Koei Tecmo lançou uma remasterização do jogo para PlayStation 4, PlayStation 5, Xbox One, Xbox Series X|S, Nintendo Switch e Microsoft Windows.[8]",
+
+        "foto" => '<img src="./fatal-cinco.jpg"></img>'
+    ]
+];
+
+$navbar = [
+    [
+        'logo' => '
+            <div>
+                <img src="./fatal-logo.jpg" alt="logotipo" class="logo">
+                <span class="titulo">FATAL FRAME: WIKI</span>
+            </div>
+
+            '
+    ],
+    [
+        'ancoras' => '
+                <ul>
+                    <li><a href="#">aokigahara</a></li>
+                    <li><a href="#">camera obskura</a></li>
+                </ul>
+                    '
+    ]
+];
+
+$page = [
+    "Fatal Frame (2001)" => [
+        "personagens" => [
+            'mafuyu' => '<img src="./MafuyuHinasaki.webp"></img>',
+            'miku' => '<img src="./Miku_Hinasaki.webp"></img>',
+            'todos' => '<img src="./irmaos-ff1.webp"></img>'
+        ],
+
+        'historia' => [
+            'mafuyu' => 'Mafuyu Hinasaki é o filho mais velho de Miyuki e Masato Hinasaki, herdou o sexto sentido da mãe, juntamente com sua irmã. Após seu pai desaparecer de forma misteriosa, sua mãe comete suicídio tempos após o desaparecimento do marido, deixando Mafuyu cuidando de sua irmã Miku Hinasaki. Ao formar-se na faculdade Mafuyu consegue um emprego em uma editora, fica amigo de Yuu Asou e Kei Amakura, juntos eles seguiam pesquisando sobre Junsei Takamine.',
+
+            'miku' => 'Miku é a filha mais nova de Masato e Miyuki Hinasaki. Ela que herdou o sexto sentido da mãe, sempre foi tímida e muito apegada a seu irmão Mafuyu Hinasaki que possui o mesmo dom. Miku perdeu o pai de forma misteriosa, por volta dos 16 ou 17 anos, Miku encontrou sua mãe enforcada no jardim da casa onde morada. Miyuki cometeu suicídio após a perda do marido, começou a apresentar sinal de loucura principalmente com a câmera obscura, herança de família. Agora orfã Miku tem somente Mafuyu como parente, ela fica mais próxima do irmão e sendo cuidada e acolhida por ele.'
+        ],  
+
+    ],
+    
+    "Fatal Frame (2003)" => [
+        "personagens" => "Fatal Frame II: Crimson Butterfly (2003)",
+
+        "local" => "Crimson Butterfly é o segundo título da série Fatal Frame e apresenta uma história independente com poucas relações com o primeiro título. A sequência foi lançada inicialmente para PlayStation 2 em 2003 na América do Norte e Japão e uma recriação desenvolvida para o Wii foi lançada em 2012 no Japão e Europa. A história segue as irmãs gêmeas Mio e Mayu Amakura enquanto exploram uma vila abandonada e experimentam encontros com criaturas paranormais. Suas vidas tornam-se ameaçadas quando os espíritos da aldeia começam a possuir o corpo de Mayu com o objetivo de realizar um sacrifício para um ritual antigo. Os jogadores devem usar uma câmera com poderes de exorcismo para derrotar inimigos e descobrir os segredos da aldeia.",
+
+        "foto" => '<img src="./fatal-dois.webp"></img>'
+    ],
+
+    "Fatal Frame (2005)" => [
+        "personagens" => "Fatal Frame III: The Tormented (2005)",
+
+        "local" => "A história segue a fotógrafa Rei Kurosawa que foi encarregada de tirar fotos de uma mansão abandonada. Depois que ela vê uma aparição de seu noivo que ela perdeu recentemente em um acidente de carro, os eventos rapidamente tomam um rumo paranormal que faz a protagonista explorar a mansão em seus sonhos e descobre de sua conexão com aqueles que sofrem de culpa sobrevivente.",
+
+        "foto" => '<img src="./fatal-tres.webp"></img>'
+    ],
+
+    "Fatal Frame (2008)" => [
+        "personagens" => "Fatal Frame: Mask of the Lunar Eclipse (2008)",
+
+        "local" => "Em 2008, Fatal Frame: Mask of the Lunar Eclipse foi lançado para Wii apenas no Japão. A Tecmo compartilhou o desenvolvimento do título com a Nintendo SPD e a Grasshopper Manufacture. O enredo do jogo se passa na Ilha Rougetsu no qual um incidente provocado por razões desconhecidas afetou um grupo de garotas que foi mantida em cativeiro. Anos após seu resgate, ainda sofrendo de amnésia, uma das garotas chamada Ruka e duas outras sobreviventes retornam à ilha para buscar a verdade. A jogabilidade, assim como nos títulos anteriores, gira em torno da personagem principal explorando ambientes e enfrentando fantasmas hostis usando a Câmera Obscura.[6][7]",
+
+        "foto" => '<img src="./fatal-quatro.webp"></img>'
+    ],
+
+    "Fatal Frame (2015)" => [
+        "personagens" => "Fatal Frame: Maiden of Black Water (2015)",
+
+        "local" => "O quinto título da franquia foi lançado para o Wii U em 2014 no Japão e em 2015 para os outros territórios. A história, que se passa na fictícia Montanha Hikami, concentra em três protagonistas que são atraídas por eventos sobrenaturais que acontecem na área, incluindo um ritual antigo para selar a Água Negra, uma força malévola que corrompe os espíritos circundantes que está ligado ao destino da donzela do santuário Ose Kurosawa. Em 2021, em comemoração do 20º aniversário da série, a Koei Tecmo lançou uma remasterização do jogo para PlayStation 4, PlayStation 5, Xbox One, Xbox Series X|S, Nintendo Switch e Microsoft Windows.[8]",
 
         "foto" => '<img src="./fatal-cinco.jpg"></img>'
     ]
