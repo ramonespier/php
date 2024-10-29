644d7af182961ac4dfb0471fdd2435666d781f3f
ramonespier <ramon.coelhomelo@outlook.com>
2024-10-29 19:28:04 -0300

    no feat: nao consigo fazer o bendito cadastro

--- login/cadastro.php
+++ login/cadastro.php
@@ -4,19 +4,11 @@ if (isset($_POST['usuario'], $_POST['senha'], $_POST['confirma'])) {
     $usuario[$_POST['usuario']];
     $senha = [$_POST['senha']];
     $confirma = [$_POST['confirma']];
-
-    if ($confirma === $senha) {
-        echo '<input class="enviar" type="submit" value="Cadastrar">';
-        
-    }
 };
-
-
 ?>
 
-
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 
 <head>
     <meta charset="UTF-8">
@@ -34,12 +26,14 @@ if (isset($_POST['usuario'], $_POST['senha'], $_POST['confirma'])) {
         <form action="cadastro.php" method="POST" class="section-form">
 
             <input class="nome-idade" type="text" name="usuario" placeholder="Insira o seu nome de usuário" required>
-            <input class="nome-idade" type="password" name="senha" placeholder="Insira sua senha" required>
-            <input class="nome-idade" type="password" name="confirma" placeholder="Confirme sua senha" required>
+            <input class="nome-idade" id="senha" type="text" name="senha" placeholder="Insira sua senha" required>
+            <input class="nome-idade" id="confirma" type="text" name="confirma" placeholder="Confirme sua senha" required>
+
 
         </form>
     </section>
 
+    <script src="./cadastro.js"></script>
 </body>
 
 </html>
